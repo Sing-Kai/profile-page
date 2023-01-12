8e86012b8ba39be29b6d3cb857c08fc138b7be69
Sing-Kai Chiu <singkai.chiu@gmail.com>
2023-01-12 22:58:36 +0000

    added title and flex box

--- profile-page-app/src/components/navbar.tsx
+++ profile-page-app/src/components/navbar.tsx
@@ -91,23 +91,48 @@ import { NavLink  } from 'react-router-dom';
 const NavBar = () => {
     return (
       <NavBarContainer>
-        <li>
-          <Link href='/'>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href='/about'>
-            About
-          </Link>
-        </li>
+        <NavBarItems>
+          <TitleContainer>
+          Sing-Kai 
+          </TitleContainer>
+          <NavBarItem>
+            <Link href='/'>
+              Home
+            </Link>
+          </NavBarItem>
+          <NavBarItem>
+            <Link href='/about'>
+              About
+            </Link>
+          </NavBarItem>
+        </NavBarItems>
       </NavBarContainer>
     )
 }
 
-const NavBarContainer = styled.ul`
-  background: red;
+const NavBarContainer = styled.div`
+  background: pink;
+  padding: 10px;
+`;
+
+const NavBarItems = styled.ul`
+  background: grey;
   color: white;
+  padding: 10px;
+  list-style-type: none;
+  display: flex;
+`;
+
+const NavBarItem = styled.li`
+  padding: 5px;
+  margin: 5px;
+`;
+
+const TitleContainer = styled.div`
+  background: red;
+  padding: 5px;
+  margin: 5px;
+  margin-right: auto;
 `;
 
 export default NavBar
