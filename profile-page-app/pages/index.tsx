50860ddb5c27dc382456599e4e711da34372ca11
Sing-Kai Chiu <singkai.chiu@gmail.com>
2023-01-17 23:08:58 +0000

    added items container

--- profile-page-app/pages/index.tsx
+++ profile-page-app/pages/index.tsx
@@ -16,12 +16,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <PageContainer>
+        <PageItems>
         <PageItem>
           <h1>Title</h1>
         </PageItem>
         <PageItem>
-          <p>Profile Page Work In Progress</p>
+          <p>Lose john poor same it case do year we. Full how way even the sigh. Extremely nor furniture fat questions now provision incommode preserved. Our side fail find like now. Discovered travelling for insensible partiality unpleasing impossible she. Sudden up my excuse to suffer ladies though or. Bachelor possible marianne directly confined relation as on he.
+
+Speedily say has suitable disposal add boy. On forth doubt miles of child. Exercise joy man children rejoiced. Yet uncommonly his ten who diminution astonished. Demesne new manners savings staying had. Under folly balls death own point now men. Match way these she avoid see death. She whose drift their fat off.
+
+Now led tedious shy lasting females off. Dashwood marianne in of entrance be on wondered possible building. Wondered sociable he carriage in speedily margaret. Up devonshire of he thoroughly insensible alteration. An mr settling occasion insisted distance ladyship so. Not attention say frankness intention out dashwoods now curiosity. Stronger ecstatic as no judgment daughter speedily thoughts. Worse downs nor might she court did nay forth these.
+
+It if sometimes furnished unwilling as additions so. Blessing resolved peculiar fat graceful ham. Sussex on at really ladies in as elinor. Sir sex opinions age properly extended. Advice branch vanity or do thirty living. Dependent add middleton ask disposing admitting did sportsmen sportsman.
+
+Nor hence hoped her after other known defer his. For county now sister engage had season better had waited. Occasional mrs interested far expression acceptance. Day either mrs talent pulled men rather regret admire but. Life ye sake it shed. Five lady he cold in meet up. Service get met adapted matters offence for. Principles man any insipidity age you simplicity understood. Do offering pleasure no ecstatic whatever on mr directly.
+          </p>
         </PageItem>
+        </PageItems>
       </PageContainer>
     </>
   )
@@ -33,8 +44,14 @@ const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  margin: 100px 100px 100px 100px;
+  margin: auto;
+  margin-top: 100px;
+  width: 60%;
   max-height: 100vh;
+`;
+
+const PageItems = styled.div`
+  margin: 50px 10px 50px 10px;
 `;
 
 const PageItem = styled.div`
