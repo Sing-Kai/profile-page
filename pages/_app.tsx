fd02cf5202af55ec533c4e0dc1be6aa9ed3388d9
Sing-Kai Chiu <singkai.chiu@gmail.com>
2023-02-20 17:01:32 +0000

    clear up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from 'styled-components'
 import {COLORS} from '../src/colours/colours'
-import styled from 'styled-components'
-import Nav from "../src/components/Nav"
 import Footer from "../src/components/Footer"
-import profile from '../src/images/profile-pic.jpg';
-import Image from 'next/image'
-import BackImage from '../src/components/BackImage'
 
 const theme = {
   colors: COLORS,
@@ -21,15 +16,3 @@ export default function App({ Component, pageProps }: AppProps) {
     </ThemeProvider>
   )
 }
-
-const Header = styled.div`
-  height: 95vh;
-  background-image: linear-gradient(
-    to right bottom, 
-    rgba(126, 213, 111, 0.8), 
-    rgba(40, 180, 131, 1.0));
-  background-size:cover;
-  position: relative;
-  clip-path:polygon(0 0, 100% 0, 100% 100%, 0 100% );
-  max-width: 100vw;
-`
